77f9c043fa9608e965a1f9d95addfb642883f61c
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-08-03 18:08:42 +0000

    More parallelization

--- run-query-w-profiling.sh
+++ run-query-w-profiling.sh
@@ -143,9 +143,9 @@ worker-setup() {
 for addr_idx in "${!ip_addr_arr[@]}"; do
     ip_addr="${ip_addr_arr[$addr_idx]}"
     ip_addr=$(echo "$ip_addr" | xargs)
-    worker-setup $ip_addr $addr_idx
+    worker-setup $ip_addr $addr_idx &
 done
-#wait
+wait
 
 time mclient -d leader-db -f raw -w 80 -i < $QUERY_FILE > results/$BASE_NAME/out
 
